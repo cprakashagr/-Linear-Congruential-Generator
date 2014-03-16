d111f54b21f61832bff3901dcd073220041db007
Chandra Prakash <cprakashagr@gmail.com>
2014-03-16 19:15:38 +0530

    Uniformity Check

--- src/com/cprakashagr/LCG.java
+++ src/com/cprakashagr/LCG.java
@@ -22,7 +22,8 @@ public class LCG {
 		 * Conditions (Not necessary, but important if LCG is considered.): 
 		 * 1. if m is divisible by 4, then a-1 is also divisible by 4.
 		 */
-		generateRandom();
+//		generateRandom();
+//		uniformityCheck();
 	}
 	
 	public double myRandom(int start, int end, double last) {
@@ -37,8 +38,29 @@ public class LCG {
 	
 	public void uniformityCheck() {
 		
-		//  Uniformity on the basis of numbers generated between 0 and 25, by checking the number of occurrence for each one of them.
+		//  Uniformity on the basis of numbers generated between 0 and 10, by checking the number of occurrence for each one of them.
+		int[] ranNum = new int[10];
+		int i=0;
+		double last = 0;
 		
+		for (i=0;i<10;i++)
+			ranNum[i] = 0;
+		
+		// Iterations: 100
+		for (i=0;i<100;i++) {
+			last = myRandom(0, 10, last);
+			int intLast = (int) last;
+			
+			if (ranNum[intLast] > 0)
+				ranNum[intLast] += 1;
+			else
+				ranNum[intLast] = 1;
+			
+			//System.out.println(last + " - " + intLast + " - " + ranNum[intLast]);
+		}
+		
+		for (i=0;i<10;i++)
+			System.out.println(ranNum[i]);
 	}
 	
 	public void generateRandom() {
