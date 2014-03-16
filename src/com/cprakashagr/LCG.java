de0975d5f5a2328cc61373b11478810c953705f5
Chandra Prakash <cprakashagr@gmail.com>
2014-03-16 18:29:54 +0530

    LCG Generation

--- src/com/cprakashagr/LCG.java
+++ src/com/cprakashagr/LCG.java
@@ -2,10 +2,36 @@ package com.cprakashagr;
 
 public class LCG {
 
+	double seed;
+	int a;
+	int c;
+	int m;
+	
 	public LCG() {
 		// TODO Auto-generated constructor stub
 		
+		seed	= Math.PI;
+		a		= 65537;
+		c		= 26493;
+		m		= 16*4;
+		myRandom();
+	}
+	
+	public double myRandom() {
 		
+		int i=0;
+		double lastNum = seed;
+		for (i=0;i<100;i++) {
+			lastNum = ((a*lastNum + c)%m)%m;
+			System.out.println(lastNum);
+		}
+		
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		
+		new LCG();
 	}
 
 }
