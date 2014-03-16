46e225b4182e7922134b8c05a680d592ff253b47
Chandra Prakash <cprakashagr@gmail.com>
2014-03-16 19:20:07 +0530

    i

--- src/com/cprakashagr/LCG.java
+++ src/com/cprakashagr/LCG.java
@@ -13,10 +13,10 @@ public class LCG {
 	
 	public LCG() {
 		// TODO Auto-generated constructor stub
-		seed	= Math.PI;
-		a		= 65537;
-		c		= 26493;
-		m		= 16*4;
+		seed	= Math.PI;	//Starting Number
+		a		= 65537;	//Multiplier
+		c		= 26493;	//Increment
+		m		= 16*4;		//Modulus
 		
 		/*
 		 * Conditions (Not necessary, but important if LCG is considered.): 
